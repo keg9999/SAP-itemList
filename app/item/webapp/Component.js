a3cfe3b047fd694c1a6a23121aa9072b2c71ec4b
Da-Won Baek <dwbaek@bsncglobal.com>
2022-07-15 06:00:39 +0000

    first project commit

---
+++ app/item/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/fe/core/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("ns.item.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
